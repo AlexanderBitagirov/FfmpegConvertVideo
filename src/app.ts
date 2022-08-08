e2fe89683975d9b3ea1b16e6a4f922e53feffbc3
DESKTOP-MJQAUR8\alexa <bitagirov@gmail.com>
2022-08-08 14:10:01 +0300

    Input Processing

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-let a = 1;
+import { PromptService } from "./core/prompt/prompt.service";
+
+ export class App {
+    async run() {
+        const res  = await(new PromptService()).input<number>('Число', 'number');
+        console.log(res);
+    }
+}
+
+const app = new App();
+app.run();
