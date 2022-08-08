99a17a67efc28274fcd1394b7d57a5c8f5957443
DESKTOP-MJQAUR8\alexa <bitagirov@gmail.com>
2022-08-08 16:32:38 +0300

    Create Command Executor(Template Method)

--- src/core/handlers/stream.handler.ts
+++ src/core/handlers/stream.handler.ts
@@ -2,6 +2,7 @@ import { ChildProcessWithoutNullStreams } from "child_process";
 import { IStreamLogger } from "./stream-logger.interface";
 
 export class StreamHandler {
+    
     constructor(private logger: IStreamLogger) {}
 
     processOutput(stream: ChildProcessWithoutNullStreams) {
